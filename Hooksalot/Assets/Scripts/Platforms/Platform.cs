03bc786d148b086fabbc51fbae4b79bd46c17925
Murkorus <95182180+Murkorus@users.noreply.github.com>
2025-10-17 11:57:45 +0200

    Make Platform Script

---
+++ Hooksalot/Assets/Scripts/Platforms/Platform.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    // This script is attached to every platform.
+    // Make it possible for the player to break platforms when they hit them if they have enough speed.
+    // Make different kinds of platforms that can withstand more or less force before they break.
+    // Also make a "checkpoint" platform that never breaks.
+    public float durability;
+
+
+}
