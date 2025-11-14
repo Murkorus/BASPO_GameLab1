2296dec91ede67ff07b2423bcf170109bee36b75
eyescreamsolbakken-droid <eyescream.solbakken@gmail.com>
2025-11-14 16:49:29 +0100

    Enemy spawn fixed

--- Hooksalot/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Hooksalot/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -6,19 +6,20 @@ public class EnemyMovement : MonoBehaviour
     public float EnemySpeed = 10f;
     public Transform playerTransform;
     private bool isChasing = false;
-
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isChasing = true;
 
     }
-
+    
     void Update()
     {
+       
         if (isChasing == true)
         {
-            Vector2 direction = (playerTransform.position - transform.position).normalized;
+            Vector2 direction = (GameManager.playerRB.transform.position - transform.position);
             transform.position += (Vector3)(direction * EnemySpeed * Time.deltaTime);
         }
     }
--- Hooksalot/Assets/Scripts/Enemies/SuicideEnemy.cs
+++ Hooksalot/Assets/Scripts/Enemies/SuicideEnemy.cs
@@ -7,8 +7,6 @@ public class SuicideEnemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
             Rigidbody2D rb = GameManager.playerRB;
-
-        
         GrapplingHook hook = GameManager.hook;
         if (hook.hookLaunched) 
         {
